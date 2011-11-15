f15b224935a429e1f2b1f97935016e0e893f35aa
drealdal <meiqizhu@gmail.com>
2011-11-15 01:07:39 -0800

    basic Make finish, including log, and test_log,begin to coding for log system

---
+++ src/include/pomme_log.h
@@ -0,0 +1,52 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  pomme_log.h
+ *
+ *    Description:   
+ *
+ *        Version:  1.0
+ *        Created:  11/08/2011 10:47:05 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  drealdal (zhumeiqi), meiqizhu@gmail.com
+ *        Company:  Tsinghua University, HPC
+ *
+ * =====================================================================================
+ */
+#ifndef POMME_LOG_H
+#define POMME_LOG_H
+#include "utils.h"
+
+#ifndef MAXLOGTRY
+#define MAXLOGTRY 3
+#endif
+
+typedef enum LOG_LEVEL{
+	POMME_LOG_ERROR,
+	POMME_LOG_WARNING,
+	POMME_LOG_INFO,
+	POMME_LOG_DEBUG,
+	POMME_LOG_MAX
+}pomme_log_level_t;
+
+typedef struct log {
+	struct timeval log_time; 
+        char *message;
+	pomme_log_level_t log_type;
+	struct list_head *log_message;	
+}log_t;
+typedef struct logger{
+	struct list_head *inuse;
+	struct list_head *freeSlots;
+}logger_t;
+void POMME_LOG(char *filename,int line,char *message,pomme_log_level_t level);
+
+#define POMME_LOG_ERROR(message) POMME_LOG(__FILE__,__LINE__,message,POMME_LOG_ERROR)
+#define POMME_LOG_WARNING(message) POMME_LOG(__FILE__,__LINE__,message,POMME_LOG_WARNING)
+#define POMME_LOG_INFO(message) POMME_LOG(__FILE__,__LINE__,message,POMME_LOG_INFO)
+#define POMME_LOG_DEBUG(message) POMME_LOG(__FILE__,__LINE__,message,POMME_LOG_DEBUG)
+
+
+#endif
---
+++ src/include/utils.h
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  utils.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  11/14/2011 06:41:57 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  drealdal (zhumeiqi), meiqizhu@gmail.com
+ *        Company:  Tsinghua University, HPC
+ *
+ * =====================================================================================
+ */
+#ifndef UTILS_H
+#define UTILS_H
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/time.h>
+#include "linklist.h"
+#endif
