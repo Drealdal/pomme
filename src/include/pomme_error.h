a33cec35bec59234b5dc00b7d5d83228741c13d0
drealdal <meiqizhu@gmail.com>
2011-12-14 00:23:54 -0800

    error file add

---
+++ src/include/pomme_error.h
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  pomme_error.h
+ *
+ *    Description: The defination of the error codes 
+ *
+ *        Version:  1.0
+ *        Created:  12/13/2011 06:29:46 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  drealdal (zhumeiqi), meiqizhu@gmail.com
+ *        Company:  Tsinghua University, HPC
+ *
+ * =====================================================================================
+ */
+#ifndef POMME_ERROR_H
+#define POMME_ERROR_H
+
+
+/*-----------------------------------------------------------------------------
+ *  general errors
+ *-----------------------------------------------------------------------------*/
+#define MALLOC_FAIL -1001
+#define MALLC_FAIL_MESSAGE "malloc memory fail"
+
+
+#endif
