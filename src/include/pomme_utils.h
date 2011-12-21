0685bffad77d2cb884377f293e86db140ced3f0b
drealdal <meiqizhu@gmail.com>
2011-12-20 18:09:14 -0800

    using debug() instead #ifdef DEBUG

--- src/include/pomme_utils.h
+++ src/include/pomme_utils.h
@@ -19,6 +19,14 @@
 #ifndef POMME_UTILS_H
 #define POMME_UTILS_H
 #include "utils.h"
+
+#ifdef DEBUG
+#define debug(msg,argc...) fprintf(stderr,msg,##argc)
+#else
+#define debug(msg,argc...) 0
+#endif
+
+
 char *pomme_time(char *buf);
 struct tm *pomme_time_all();
 #endif
