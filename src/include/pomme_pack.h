c86de6262576ebf66024db93220ebf599d87ce42
drealdal <meiqizhu@gmail.com>
2012-01-12 19:31:07 -0800

    add pack files

---
+++ src/include/pomme_pack.h
@@ -0,0 +1,20 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  pomme_pack.h
+ *
+ *    Description:  pach data for transport and storage in the database
+ *
+ *        Version:  1.0
+ *        Created:  01/12/2012 07:29:46 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  drealdal (zhumeiqi), meiqizhu@gmail.com
+ *        Company:  Tsinghua University, HPC
+ *
+ * =====================================================================================
+ */
+#ifndef _POMME_PACK_H
+#define _POMME_PACK_H
+#endif
