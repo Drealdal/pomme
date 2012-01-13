5bc1e3dfabb772316f79c76f8f8d4647041f24a7
Drealdal <meiqizhu@gmail.com>
2012-01-13 11:16:59 +0800

    begin meta and communication

--- src/server/include/pomme_meta.h
+++ src/server/include/pomme_meta.h
@@ -17,11 +17,15 @@
  */
 #ifndef POMME_META_H
 #define POMME_META_H
+#include "pomme_type.h"
 
 #define MAX_STORE_FILE_NAME_LENGTH 256
 
 typedef struct pomme_object
 {
+
+	u_int8 *path;
+	u_int8 *name;
 	size_t	sfid;// the id of the storefile
 	u_int64 offset;// the offset of the object in the file
 	u_int64 length;// the dataLength of the object
