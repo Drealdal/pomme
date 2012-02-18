db66c1949742e7a3fb7f6f534e774be943b31d46
drealdal <meiqizhu@gmail.com>
2012-02-17 19:31:49 -0800

    add file

---
+++ src/server/pomme_data_server.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  pomme_data_server.c
+ *
+ *    Description:  the implement of the pomme data server
+ *
+ *        Version:  1.0
+ *        Created:  02/17/2012 07:15:45 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  drealdal (zhumeiqi), meiqizhu@gmail.com
+ *        Company:  Tsinghua University, HPC
+ *
+ * =====================================================================================
+ */
+#include "pomme_storage.h"
+int main()
+{
+    int ret = 0;
+err:
+    return ret;
+}
