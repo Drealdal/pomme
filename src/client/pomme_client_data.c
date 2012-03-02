9fd812add36ce3e841a73135404bb00b9accc8d6
drealdal <meiqizhu@gmail.com>
2012-03-01 18:42:25 -0800

    client here

---
+++ src/client/pomme_client_data.c
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  pomme_client_data.c
+ *
+ *    Description:  The client logic for data server
+ *
+ *        Version:  1.0
+ *        Created:  03/01/2012 06:03:53 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  drealdal (zhumeiqi), meiqizhu@gmail.com
+ *        Company:  Tsinghua University, HPC
+ *
+ * =====================================================================================
+ */
+#include "pomme_client_data.h"
+
