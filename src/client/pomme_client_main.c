6a9d3519a895ef7bac736a74a10fa6eec3ab081e
drealdal <meiqizhu@gmail.com>
2012-03-07 00:41:14 -0800

    add fiel

---
+++ src/client/pomme_client_main.c
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  pomme_client_main.c
+ *
+ *    Description:  the main 
+ *
+ *        Version:  1.0
+ *        Created:  03/07/2012 12:17:23 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  drealdal (zhumeiqi), meiqizhu@gmail.com
+ *        Company:  Tsinghua University, HPC
+ *
+ * =====================================================================================
+ */
+#include "pomme_client_data.h"
+int main()
+{
+    return 0;
+}
