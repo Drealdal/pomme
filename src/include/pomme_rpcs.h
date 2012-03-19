5d15f04d59781f93e382691bbefb2df60d473576
drealdal <meiqizhu@gmail.com>
2012-03-19 01:47:31 -0700

    thread pool add

---
+++ src/include/pomme_rpcs.h
@@ -0,0 +1,57 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  pomme_rpc_server.h
+ *
+ *    Description:  the implement of pomme_rpc
+ *
+ *        Version:  1.0
+ *        Created:  03/18/2012 08:15:14 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  drealdal (zhumeiqi), meiqizhu@gmail.com
+ *        Company:  Tsinghua University, HPC
+ *
+ * =====================================================================================
+ */
+#ifndef _POMME_RPC_SERVER_H
+#define _POMME_RPC_SERVER_H
+#include "pomme_blist.h"
+#include "utils.h"
+typedef struct pomme_func
+{
+    char *name;
+    void *pointer;
+    pomme_link_t next;
+}pomme_func_t;
+
+typedef struct pomme_rpcs pomme_rpcs_t;
+
+typedef struct pomme_rpcs
+{
+    int version; //the version of the rpc server
+    int is_start;
+    pomme_link_t func; 
+    /*regist an function*/ 
+    int  (*func_register) (pomme_rpcs_t *rpcs,char *,void *);
+    /** print all the function */
+    void (*func_print)(pomme_rpcs_t *rpcs);
+    /**  start the server */
+    void (*start)(pomme_rpcs_t *rpcs);
+};
+
+/**
+ * @brief pomme_rpcs_init 
+ * @param rpcs
+ * @return < 0 failure, = 0 for success 
+ */
+int pomme_rpcs_init( pomme_rpcs_t *rpcs);
+/**
+ * @brief pomme_rpcs_distroy 
+ * @param rpcs
+ * @return <0 failure, ==0 for success 
+ */
+int pomme_rpcs_distroy( pomme_rpcs_t *rpcs);
+
+#endif
