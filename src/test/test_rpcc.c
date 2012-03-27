196844c593abe97322a1bad33b8a6423ed3e6d8b
drealdal <meiqizhu@gmail.com>
2012-03-27 01:37:54 -0700

    modify test_rpcc.c

--- src/test/test_rpcc.c
+++ src/test/test_rpcc.c
@@ -16,8 +16,32 @@
  * =====================================================================================
  */
 #include "pomme_rpcc.h"
+#include "pomme_const.h"
+#include "utils.h"
 
 int main()
 {
+    rpcc_t rpcc;
+    if( (ret = pomme_rpcc_init(&rpcc, "127.0.0.1",
+	    POMME_RPC_PORT,0)) < 0 )
+    {
+	debug("init rpc client error");
+	exit(-1);
+    }
+    pomme_data_t *arg = malloc(2*sizeof(pomme_data_t));
+    char *name = "remote_print";
+    arg[0].size=strlen(name);
+    arg[0].data = name;
+
+    arg[1].size = strlen(name);
+    arg[1].data = name;
+
+    pomme_data_t res;
+    rpcc.sysc_call(&rpcc, arg,&res,0); 
+    printf("Get from server:%s",res.data);
+
+    free(arg);
+    pomme_data_distroy(&res);
+
     return ret;
 }
